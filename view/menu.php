88099470cf4486460edfe662a8564c506b5bfc64
lio990527 <lio990527@163.com>
2019-06-28 15:56:08 +0800

    menu

--- view/menu.php
+++ view/menu.php
@@ -1,100 +1,79 @@
 <?php
-define('_WWW_ROOT' , dirname(dirname(__FILE__)));
-define('_PATH_SEPARATOR', preg_match("/WIN/i",PHP_OS) ? ";" : ":");
-define('_VIEW_PATH', _WWW_ROOT.'/view/');
-define('SCRIPT_ROOT',dirname(__FILE__).'/');
-ini_set("include_path", "."._PATH_SEPARATOR._WWW_ROOT.'/lib/');
+
+define('_WWW_ROOT', dirname(dirname(__FILE__)));
+define('_PATH_SEPARATOR', preg_match('/$WIN/i', PHP_OS) ? ";" : ":");
+define('_VIEW_PATH', _WWW_ROOT . '/view/');
+define('SCRIPT_ROOT', dirname(__FILE__) . '/');
+ini_set("include_path", "." . _PATH_SEPARATOR . _WWW_ROOT . '/lib/');
 
 require_once 'class/comm/class.file.php';
 require_once 'class/user/class.user.php';
 
 $objUser = new User();
-$objUser->checkAuth();
+// $objUser->checkAuth();
 $strUips = $objUser->getUserIp();
-if($_POST['menuName'] && $_POST['menuUrl'] && $_POST['todo'] !== null){
-	$menu[0] = $_POST['menuName'];
-	$menu[1] = $_POST['menuUrl'];
-	$menu[2] = $_POST['menuColor'];
-	$menu[3] = $_POST['menuType'] ? trim($_POST['menuType']) : '未命名';
-	$menu[4] = $strUips;
-	$menu[5] = $_POST['menuTarget'];
-	$menu[6] = $_POST['menuPublic'];
-	$info = join('|', $menu)."\n";
-	if($_POST['todo'] === 'upd'){
-		$menu = File::getFileInfo('../lib/conf','menu.conf');
+if ($_POST['menuName'] && $_POST['menuUrl'] && $_POST['todo'] !== null) {
+	$data = [
+		$_POST['menuName'],
+		$_POST['menuUrl'],
+		$_POST['menuColor'],
+		$_POST['menuType'] ? trim($_POST['menuType']) : '未命名',
+		$strUips,
+		$_POST['menuTarget'],
+		$_POST['menuPublic']
+	];
+
+	$info = implode('|', $data) . "\n";
+	if ($_POST['todo'] === 'upd') {
+		$menu = File::getFileInfo('../lib/conf', 'menu.conf');
 		$menu[$_GET['chg']] = $info;
-		$info = join('',$menu);
-		File::writeFile('../lib/conf','menu.conf', $info, true);
-	}else{
-		File::writeFile('../lib/conf','menu.conf', $info);
+		$info = implode("\n", $menu);
+		File::writeFile('../lib/conf', 'menu.conf', $info, true);
+	} else {
+		File::writeFile('../lib/conf', 'menu.conf', $info);
 	}
-	Header("Location:".$_SERVER['SCRIPT_URI']);
-}else if($_GET['del'] !== null){
-	$menu = File::getFileInfo('../lib/conf','menu.conf');
+	Header("Location:" . $_SERVER['SCRIPT_NAME']);
+} else if ($_GET['del'] !== null) {
+	$menu = File::getFileInfo('../lib/conf', 'menu.conf');
 	unset($menu[$_GET['del']]);
-	$info = join('',$menu);
-	File::writeFile('../lib/conf','menu.conf', $info, true);
-	Header("Location:".$_SERVER['SCRIPT_URI']);
-}else if($_GET['chg'] !== null){
+	$info = implode("\n", $menu);
+	File::writeFile('../lib/conf', 'menu.conf', $info, true);
+	Header("Location:" . $_SERVER['SCRIPT_NAME']);
+} else if ($_GET['chg'] !== null) {
 	$info = File::getFileInfo('../lib/conf', 'menu.conf', '|', $_GET['chg']);
-}else if($_GET['top'] !== null){
-	$index = $_GET['top'];
-	$menu  = File::getFileInfo('../lib/conf','menu.conf');
-	$info1 = $menu[$index];
-	$info2 = $menu[$index+1];
-	$menu[$index  ] = $info2;
-	$menu[$index+1] = $info1;
-	$info = join('',$menu);
-	File::writeFile('../lib/conf','menu.conf', $info, true);
-	Header("Location:".$_SERVER['SCRIPT_URI']);
-}else if($_GET['btm'] !== null){
-	$index = $_GET['btm'];
-	$menu  = File::getFileInfo('../lib/conf','menu.conf');
-	$info1 = $menu[$index-1];
-	$info2 = $menu[$index];
-	$menu[$index  ] = $info1;
-	$menu[$index-1] = $info2;
-	$info = join('',$menu);
-	File::writeFile('../lib/conf','menu.conf', $info, true);
-	Header("Location:".$_SERVER['SCRIPT_URI']);
 }
 
-if(false){
-	$menu = File::getFileInfo('../lib/conf', 'menu.conf', '|');
-}else{
-	$rows = File::getFileInfo('../lib/conf','menu.conf');
-	foreach ($rows as $r_k => $r_v){
+if (true) {
+	$rows = File::getFileInfo('../lib/conf', 'menu.conf');
+	foreach ($rows as $r_k => $r_v) {
 		$row = explode('|', trim($r_v));
 
-		if($row[6] === '0' && $row['4'] != $strUips){
+		if (isset($row[6]) && $row[6] === '0' && $row['4'] != $strUips) {
 			continue;
 		}
 
-		$ico = substr($row[1], strpos($row[1], '://')+3);
-
+		$ico = substr($row[1], strpos($row[1], '://') + 3);
 		$ico = (strpos($ico, '/') === false) ? $ico : substr($ico, 0, strpos($ico, '/'));
-
 		$yum = substr($ico, strrpos($ico, '.'));
 		$ico = substr($ico, 0, strrpos($ico, '.'));
-
-		$ico = (strrpos($ico,'.') === false) ? $ico : substr($ico, strrpos($ico, '.')+1);
+		$ico = (strrpos($ico, '.') === false) ? $ico : substr($ico, strrpos($ico, '.') + 1);
 
 		$row['id'] = $r_k;
-		$row['ico'] = strpos($row[1], '://') ? 'http://www.'.$ico.$yum.'/favicon.ico' : '/favicon.ico';
+		$row['ico'] = strpos($row[1], '://') ? 'http://www.' . $ico . $yum . '/favicon.ico' : '/favicon.ico';
 		$menu[$row[3]][] = $row;
 	}
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="zh">
 <head>
-	<title>网盟菜单</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<link rel="stylesheet" type="text/css" href="source/css/menu.css"/>
-	<script type="text/javascript" src="source/js/jquery-1.7.2.min.js"></script>
-	<script type="text/javascript" src="source/js/comm.js"></script>
-	<script type="text/javascript" src="source/js/jscolor/jscolor.js"></script>
-	<script type="text/javascript">
+<title>网盟菜单</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="source/css/menu.css" />
+<script type="text/javascript" src="source/js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="source/js/comm.js"></script>
+<script type="text/javascript" src="source/js/jscolor/jscolor.js"></script>
+<script type="text/javascript">
 	$(function(){
 		$('div.menu ul').mouseover(function(){
 			$(this).find('span').eq(0).addClass('btn_edt').attr('title','修改').click(function(){location.href = '?chg='+$(this).attr('key');});
@@ -156,54 +135,88 @@ if(false){
 	</script>
 </head>
 <body>
-<div class="shadow" id="w_show" <?php if(!$info){?>style="display:none;"<?php }?>></div>
-<div class="f_box"  id="w_box"  <?php if(!$info){?>style="display:none;"<?php }?> align="center">
-<form name="menuForm" id="menuForm" method="post">
-<table class="menuTab" cellspacing="0" cellpadding="0">
-  <tr><th colspan="2">添加菜单</th></tr>
-  <tr><td>菜单名称：</td><td><input<?php if($info){?> value="<?php echo $info[0]?>"<?php }?> name="menuName"/></td></tr>
-  <tr><td>菜单地址：</td><td><input<?php if($info){?> value="<?php echo $info[1]?>"<?php }?> name="menuUrl"/></td></tr>
-  <tr><td>菜单组别：</td>
-  	  <td><select name="menuType">
-  	  	<?php foreach($menu as $type => $val){ ?>
-  	  	<option value="<?php echo $type;?>" <?php if($info[3] == $type){echo 'selected="selected"';}?>><?php echo $type;?></option>
-  	  	<?php }?>
-  	  	<option value="自定义">自定义</option>
-  	  </select></td>
-  </tr>
-  <tr><td>新  窗   口：</td><td><input name="menuTarget" type="radio" value="1"<?php if($info[5] == 1){?> checked="checked"<?php }?>/>是 <input type="radio" name="menuTarget" value="0"<?php if($info[5] == 0){?> checked="checked"<?php }?>>否</td></tr>
-  <tr><td title="公共化的标签则会被所有人看到，而私有化只能是自己看到.">公  共   化：</td><td><input name="menuPublic" type="radio" value="1"<?php if($info[6] == 1){?> checked="checked"<?php }?>/>是 <input type="radio" name="menuPublic" value="0"<?php if($info[6] == 0){?> checked="checked"<?php }?>>否</td></tr>
-  <tr><td>字体颜色：</td><td><input<?php if($info){?> value="<?php echo $info[2]?>"<?php }?> name="menuColor" class="color" value="#666666"/></td></tr>
-  <tr>
-	<td align="center" colspan="2"><input type="hidden" name="todo" value="<?php echo $info ? 'upd' : 'add'; ?>"/><input type="submit" value="确定"/>&nbsp; <input type="button" value="取消" onclick="location.href='/menu.php'"/></td>
-  </tr>
-</table>
-</form>
-</div>
-
-<div class="my_menu">
-	<div class="title">我的导航
-		<span class="btn btn_mng" title="管理" onclick="mnag()">+</span>
-		<span class="btn btn_add" title="新建" onclick="hidn()">+</span>
+	<div class="shadow" id="w_show" style="display: <?php if(!$info) echo 'none';?>;"></div>
+	<div class="f_box" id="w_box" style="display: <?php if(!$info) echo 'none';?>;" align="center">
+		<form name="menuForm" id="menuForm" method="post">
+			<table class="menuTab" cellspacing="0" cellpadding="0">
+				<tr>
+					<th colspan="2">添加菜单</th>
+				</tr>
+				<tr>
+					<td>菜单名称：</td>
+					<td><input value="<?php if($info) echo $info[0];?>" name="menuName" /></td>
+				</tr>
+				<tr>
+					<td>菜单地址：</td>
+					<td><input value="<?php if($info) echo $info[1];?>" name="menuUrl" /></td>
+				</tr>
+				<tr>
+					<td>菜单组别：</td>
+					<td>
+						<select name="menuType">
+							<?php foreach($menu as $type => $val):?>
+							<option value="<?php echo $type;?>"<?php if($info[3] == $type){echo 'selected="selected"';}?>><?php echo $type;?></option>
+							<?php endforeach;?>
+							<option value="自定义">自定义</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>新 窗 口：</td>
+					<td>
+						<input name="menuTarget" type="radio" value="1"<?php if($info[5] == 1){?> checked="checked" <?php }?> />是 
+						<input name="menuTarget" type="radio" value="0"<?php if($info[5] == 0){?> checked="checked" <?php }?> />否
+					</td>
+				</tr>
+				<tr>
+					<td title="公共化的标签则会被所有人看到，而私有化只能是自己看到.">公 共 化：</td>
+					<td>
+						<input name="menuPublic" type="radio" value="1" <?php if($info[6] == 1){?> checked="checked" <?php }?> />是 
+						<input name="menuPublic" type="radio" value="0" <?php if($info[6] == 0){?> checked="checked" <?php }?> />否
+					</td>
+				</tr>
+				<tr>
+					<td>字体颜色：</td>
+					<td><input value="<?php if($info) echo $info[2];?>" name="menuColor" class="color" value="#666666" /></td>
+				</tr>
+				<tr>
+					<td align="center" colspan="2">
+						<input type="hidden" name="todo" value="<?php echo $info ? 'upd' : 'add'; ?>" />
+						<input type="submit" value="确定" />&nbsp; 
+						<input type="button" value="取消" onclick="location.href='/menu.php'" />
+					</td>
+				</tr>
+			</table>
+		</form>
 	</div>
-	<?php foreach($menu as $type => $val){?>
-	<div class="line">
-		<div class="type"><?php echo $type;?></div>
-		<div class="menu">
-			<?php foreach($val as $k => $v){  $v[1]=preg_replace('/date=\d{4}-\d{2}-\d{2}/','date='.date('Y-m-d',strtotime('+2 day')), $v[1]);?>
-			<ul>
-				<a href="<?php echo $v[1]?>"<?php if($v[5] == 1){ echo ' target="_blank"';} ?>><img name="menu_ico" src="" width="16" height="16" data="<?php echo $v['ico']?>"/></a>
-				<li>
-					<a href="<?php echo $v[1]?>" title="<?php echo $v[0]?>" style="color:#<?php echo $v[2]?>" <?php if($v[5] == 1){ echo ' target="_blank"';} ?>><?php echo $v[0]?></a>
-				</li>
-				<span class="btn edit" key="<?php echo $v['id'];?>">+</span>
-				<span class="btn delet" key="<?php echo $v['id'];?>">+</span>
-			</ul>
-			<?php }?>
+
+	<div class="my_menu">
+		<div class="title">
+			我的导航
+			<span class="btn btn_mng" title="管理" onclick="mnag()">+</span> 
+			<span class="btn btn_add" title="新建" onclick="hidn()">+</span>
+		</div>
+		<?php foreach($menu as $type => $val):?>
+		<div class="line">
+				<div class="type"><?php echo $type;?></div>
+				<div class="menu">
+				<?php foreach($val as $v):?>
+				<ul>
+					<a href="<?php echo $v[1]?>" target="<?php if($v[5] == 1)echo '_blank';?>">
+						<img name="menu_ico" width="16" height="16" data="<?php echo $v['ico']?>" />
+					</a>
+					<li><a href="<?php echo $v[1]?>" title="<?php echo $v[0]?>" style="color:#<?php echo $v[2]?>" target="<?php if($v[5]) echo '_blank';?>"><?php echo $v[0]?></a></li>
+					<span class="btn edit" key="<?php echo $v['id'];?>">+</span>
+					<span class="btn delet" key="<?php echo $v['id'];?>">+</span>
+				</ul>
+				<?php endforeach;?>
+			</div>
+		</div>
+		<?php endforeach;?>
+		<div class="line">
+			<input id="w_link" onkeydown="Jump()" style="margin: 3px 5px; width: 728px;" />
+			<button onclick="linkBlank()" style="float: right; margin-right: 5px;">猛戳</button>
 		</div>
 	</div>
-	<?php }?>
-	<div class="line"><input id="w_link" onkeydown="Jump()" style="margin:3px 5px;width:728px;"/><button onclick="linkBlank()" style="float:right;margin-right: 5px;">猛戳</button></div>
-</div>
 </body>
 </html>
